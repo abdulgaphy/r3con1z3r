846c6dff575e5c7c394d98486e374d01819b7bf8
Deven96 <diretnandomnan@gmail.com>
2019-05-18 17:24:05 +0100

    tailored os independent sequence

--- .travis/before_script.sh
+++ .travis/before_script.sh
@@ -19,13 +19,13 @@ fi
 
 if [[ "$TRAVIS_OS_NAME" = "windows" ]]; then 
     if [[ "$TRAVIS_PYTHON_VERSION" == "27" ]]; then
-        choco install python27
+        choco install python --version 2.7
         export PATH="/c/Python27:/c/Python27/Scripts:$PATH"
     elif [[ "$TRAVIS_PYTHON_VERSION" == "35" ]]; then
-            choco install python35
+            choco install python --version 3.5.2
             export PATH="/c/Python35:/c/Python35/Scripts:$PATH"
     elif [[ "$TRAVIS_PYTHON_VERSION" == "36" ]]; then
-            choco install python36
+            choco install python --version 3.6.5
             export PATH="/c/Python36:/c/Python36/Scripts:$PATH"
     fi
     python -m pip install --upgrade pip wheel
