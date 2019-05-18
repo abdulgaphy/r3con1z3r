8bbfd36c0cb3f46e24c0c2e31cb06d462ba0bb9d
Deven96 <diretnandomnan@gmail.com>
2019-05-18 17:36:44 +0100

    edited sh script

--- .travis/before_script.sh
+++ .travis/before_script.sh
@@ -19,16 +19,16 @@ fi
 
 if [[ "$TRAVIS_OS_NAME" = "windows" ]]; then 
     if [[ "$TRAVIS_PYTHON_VERSION" == "27" ]]; then
-        choco install python2
-        export PATH="/c/Python27:/c/Python27/Scripts:$PATH"
+        choco install python2;
+        export PATH="/c/Python27:/c/Python27/Scripts:$PATH";
     elif [[ "$TRAVIS_PYTHON_VERSION" == "35" ]]; then
-            choco install python --version 3.5.2
-            export PATH="/c/Python35:/c/Python35/Scripts:$PATH"
+            choco install python --version 3.5.2;
+            export PATH="/c/Python35:/c/Python35/Scripts:$PATH";
     elif [[ "$TRAVIS_PYTHON_VERSION" == "36" ]]; then
-            choco install python --version 3.6.5
-            export PATH="/c/Python36:/c/Python36/Scripts:$PATH"
+            choco install python --version 3.6.5;
+            export PATH="/c/Python36:/c/Python36/Scripts:$PATH";
     fi
     python -m pip install --upgrade pip wheel
     # install unicode and colorama
-    pip install win_unicode_console colorama; fi
+    pip install win_unicode_console colorama;
 fi
