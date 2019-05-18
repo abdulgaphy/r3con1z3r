496b47c193ca3cc1fd269445b57b381ff248d68e
Deven96 <diretnandomnan@gmail.com>
2019-05-18 18:19:55 +0100

    attempting fixes

--- .travis/before_script.sh
+++ .travis/before_script.sh
@@ -4,16 +4,17 @@ set -v
 if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then brew update; fi
 if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then 
     if [[ "$TRAVIS_PYTHON_VERSION" == "27" ]]; then
-        brew install python;
-        virtualenv venv -p python;
+        /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)";
+        echo 'export PATH="/usr/local/bin:/usr/local/sbin:$PATH"' >> ~/.profile;
+        brew install python@2
+        ln -s pip="python2.7 -m pip"
     elif [[ "$TRAVIS_PYTHON_VERSION" == "35" ]]; then
         brew install sashkab/python/python35;
-        virtualenv venv -p python3.5;
+        echo 'export PATH="/usr/local/opt/python35/bin:$PATH"' >> ~/.bash_profile;
+        ln -s pip=pip3.5;
     elif [[ "$TRAVIS_PYTHON_VERSION" == "36" ]]; then
-        brew install sashkab/python/python36;
-        virtualenv venv -p python3.6;
+        :
     fi
-    source venv/bin/activate;
 fi
 
 
@@ -25,7 +26,7 @@ if [[ "$TRAVIS_OS_NAME" = "windows" ]]; then
             choco install python3 --version 3.5.2;
             export PATH="/c/Python35:/c/Python35/Scripts:$PATH";
     elif [[ "$TRAVIS_PYTHON_VERSION" == "36" ]]; then
-            choco install python3 --version 3.6.5;
+            choco install python --version 3.6.5;
             export PATH="/c/Python36:/c/Python36/Scripts:$PATH";
     fi
     python -m pip install --upgrade pip wheel
