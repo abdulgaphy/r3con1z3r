765c2acca344a7a2a100dbe77fc447454953d558
Deven96 <diretnandomnan@gmail.com>
2019-05-18 17:14:52 +0100

    merged upstream/master

---
+++ .travis/before_script.sh
@@ -0,0 +1,34 @@
+set -e
+set -v
+
+if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then brew update; fi
+if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then 
+    if [[ "$TRAVIS_PYTHON_VERSION" == "27" ]]; then
+        brew install python;
+        virtualenv venv -p python;
+    elif [[ "$TRAVIS_PYTHON_VERSION" == "35" ]]; then
+        brew install sashkab/python/python35;
+        virtualenv venv -p python3.5;
+    elif [[ "$TRAVIS_PYTHON_VERSION" == "36" ]]; then
+        brew install sashkab/python/python36;
+        virtualenv venv -p python3.6;
+    fi
+    source venv/bin/activate;
+fi
+
+
+if [[ "$TRAVIS_OS_NAME" = "windows" ]]; then 
+    if [[ "$TRAVIS_PYTHON_VERSION" == "27" ]]; then
+        choco install python27
+        export PATH="/c/Python27:/c/Python27/Scripts:$PATH"
+    elif [[ "$TRAVIS_PYTHON_VERSION" == "35" ]]; then
+            choco install python35
+            export PATH="/c/Python35:/c/Python35/Scripts:$PATH"
+    elif [[ "$TRAVIS_PYTHON_VERSION" == "36" ]]; then
+            choco install python36
+            export PATH="/c/Python36:/c/Python36/Scripts:$PATH"
+    fi
+    python -m pip install --upgrade pip wheel
+    # install unicode and colorama
+    pip install win_unicode_console colorama; fi
+fi
