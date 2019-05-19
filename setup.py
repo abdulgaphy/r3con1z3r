2925d07183f525f64bc65b88241f241bae5b56ed
Raji Abdulgafar <30292855+abdulgaphy@users.noreply.github.com>
2019-05-19 15:52:30 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ requires = [line.strip('\n') for line in open(REQUIREMENTS).readlines()]
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-VERSION = "1.0.2"
+VERSION = "1.0.3"
 
 setuptools.setup(
     name="R3con1z3r",
